fde757729a6da23a81750abad5fdce07f945e479
Kenshi <tkmt518@gmail.com>
2022-01-22 16:32:31 +0900

    CHANGED TO MORE COMPACT - USING :NOT CODE

--- 選択で大きくなる/index.js
+++ 選択で大きくなる/index.js
@@ -5,22 +5,21 @@ $(function(){
 
   $(".first-box").click( function(event){
     $(this).css('transform', big);
-    $(".second-box").css('transform', normal);
-    $(".third-box").css('transform', normal);
-//     :not で簡潔に書けるかも。 
+    // $(".second-box").css('transform', normal);
+    // $(".third-box").css('transform', normal);
+//     :not で簡潔に書けるかも。
+    $('.box').not(this).css('transform', normal);
     $(".text-space").html("<p>これは1のテキスト</p>");
   });
   $(".second-box").click( function(event){
     $(this).css('transform', big);
-    $(".first-box").css('transform', normal);
-    $(".third-box").css('transform', normal);
+    $('.box').not(this).css('transform', normal);
     $(".text-space").html("<p>これは2のテキスト</p>");
 
   });
   $(".third-box").click( function(event){
     $(this).css('transform', big);
-    $(".second-box").css('transform', normal);
-    $(".first-box").css('transform', normal);
+    $('.box').not(this).css('transform', normal);
     $(".text-space").html("<p>これは3のテキスト</p>");
   });
 
